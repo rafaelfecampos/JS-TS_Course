3aba6bbb73e90db95f01edebc93cfb0c6f4fad6e
Rafael Ferreira Campos <2001campos.rafael@gmail.com>
2026-02-04 20:56:35 -0300

    First commit

---
+++ secao2/aula1/index.js
@@ -0,0 +1,12 @@
+console.log("Hello 'World'!"); //utilizar '' dentro da string
+console.log('"Botafogo"'); //utilizar "" dentro da string
+console.log(`de "Futebol" e 'Calotes'`); //ambas as aspas dentro da string
+
+console.log(123456, 123.456, 'BFR'); //exibido o número como number
+console.log(123456, typeof(123456));
+console.log(123.456, typeof(123.456));
+console.log("BFR", typeof("BFR"));
+
+
+
+
